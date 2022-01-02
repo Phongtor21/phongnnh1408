6db5c846444bb9df5debd94747a343445dcb3168
Tue666 <dapamu333@gmail.com>
2022-01-02 20:12:07 +0700

    edit custom cors to cors npm

--- Server/src/index.js
+++ Server/src/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const path = require('path');
 const initialRoutes = require('./routes');
 const db = require('./config/db');
-const cors = require('./config/cors');
+// cors npm
+const cors = require('cors');
+// Custom cors
+// const cors = require('./config/cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +15,7 @@ db.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors);
+app.use(cors());
 app.use('/images', express.static(path.join(__dirname, '../uploads')));
 
 initialRoutes(app);
